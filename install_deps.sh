8600a67c48f0463c2d94e470d939c0510a0071ba
Doug Raum <draum@wirelessanalytics.com>
2017-03-07 22:25:15 -0500

    Add thefuck

--- install_deps.sh
+++ install_deps.sh
@@ -2,6 +2,9 @@
 
 echo "Installing some dependancies, in case they aren't here."
 
-sudo apt-get install emacs24-nox emacs-goodies-el zsh
+sudo apt-get install emacs24-nox emacs-goodies-el zsh 
+sudo apt-get install python-pip python-dev
+sudo pip install thefuck
+
 curl -L http://install.ohmyz.sh | sh
 
