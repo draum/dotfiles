c2a3e375d35c16deaf7a8821ea8b9abbf0eff8a2
Doug Raum <draum@wirelessanalytics.com>
2017-03-07 21:48:57 -0500

    Bump to emacs24-nox

--- install_deps.sh
+++ install_deps.sh
@@ -2,6 +2,6 @@
 
 echo "Installing some dependancies, in case they aren't here."
 
-sudo apt-get install emacs23-nox emacs-goodies-el zsh
+sudo apt-get install emacs24-nox emacs-goodies-el zsh
 curl -L http://install.ohmyz.sh | sh
 
