b9eb414be27f4b796d16eade328479730030fe71
Caede <caede.x@gmail.com>
2017-03-08 03:34:12 +0000

    Fixes and cleanup

--- install_deps.sh
+++ install_deps.sh
@@ -4,7 +4,7 @@ echo "Installing some dependancies, in case they aren't here."
 
 sudo apt-get install emacs24-nox emacs-goodies-el zsh 
 sudo apt-get install python-pip python-dev
-sudo pip install thefuck
-
+sudo pip install -U pip
+sudo pip install psutil thefuck
 curl -L http://install.ohmyz.sh | sh
 
